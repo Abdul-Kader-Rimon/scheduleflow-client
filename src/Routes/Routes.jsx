c87afa38e000a1815bf9a40baee7615a24a8c0d1
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2026-04-28 02:02:08 +0600

    fix: correct Signup.jsx import case in Routes.jsx

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 import Dashboard from '../components/dashboard/Dashboard.jsx';
 import RootLayout from '../publicLayout/PublicLayout.jsx';
-import Signup from '../components/signup/signup.jsx';
+import Signup from '../components/signup/Signup.jsx';
 import DashboardLayout from '../components/dashboardlayout/DashboardLayout.jsx';
 import Login from '../components/login/Login.jsx';
 import Home from '../components/home/Home.jsx';
